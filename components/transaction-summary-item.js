7dbaf106b3691bafa10e8c5c83edcba41eae98f2
d.parubochyi <d.parubochy@gmail.com>
2025-09-11 11:04:03 +0300

    Transaction loading in suspense

--- components/transaction-summary-item.js
+++ components/transaction-summary-item.js
@@ -1,11 +1,13 @@
-import { useFormatCurrency } from "@/hooks/use-format-currency"
+import { useFormatCurrency } from '@/hooks/use-format-currency'
 
-export default function TransactionSummaryItem({date, amount}) {
+export default function TransactionSummaryItem({ date, amount }) {
     const formatedAmount = useFormatCurrency(amount)
 
-    return (<div className="flex text-gray-500 dark:text-gray-400 font-semibold">
-        <div className="grow">{date}</div>
-        <div className="min-w-[70px] text-right font-semibold">{formatedAmount}</div>
-        <div className="min-w-[50px]"></div>
-    </div>)
+    return (
+        <div className="flex text-gray-500 dark:text-gray-400 font-semibold">
+            <div className="grow">{date}</div>
+            <div className="min-w-[70px] text-right font-semibold">{formatedAmount}</div>
+            <div className="min-w-[50px]"></div>
+        </div>
+    )
 }
