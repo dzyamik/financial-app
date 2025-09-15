5bdfae296994fb7cfa96617d66ba845a6584b243
d.parubochyi <d.parubochy@gmail.com>
2025-09-15 09:18:40 +0300

    RLS, policies, uaser accesses

--- seed.mjs
+++ seed.mjs
@@ -6,16 +6,49 @@ dotenv.config({ path: '.env.local' })
 
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE)
 
-export const categories = ['Housing', 'Transport', 'Health', 'Food', 'Education', 'Other']
+const categories = ['Housing', 'Transport', 'Health', 'Food', 'Education', 'Other']
+
+async function seedUsers() {
+    for (let i = 0; i < 5; i++) {
+        try {
+            const { error } = await (
+                await supabase
+            ).auth.admin.createUser({
+                email: faker.internet.email(),
+                password: 'password',
+            })
+
+            if (error) {
+                throw new Error(error)
+            }
+
+            console.log(`User added`)
+        } catch (e) {
+            console.error(`Error adding user`)
+        }
+    }
+}
 
 async function seed() {
+    await seedUsers()
     let transactions = []
+    const {
+        data: { users },
+        error: listUsersError,
+    } = await (await supabase).auth.admin.listUsers()
 
-    for (let i = 0; i < 10; i++) {
+    if (listUsersError) {
+        console.error(`Cannot list users, aborting`)
+        return
+    }
+
+    const userIds = users?.map((user) => user.id)
+
+    for (let i = 0; i < 100; i++) {
         const created_at = faker.date.past()
         let type,
             category = null
-
+        const user_id = faker.helpers.arrayElement(userIds)
         const typeBias = Math.random()
 
         if (typeBias < 0.8) {
@@ -24,21 +57,29 @@ async function seed() {
         } else if (typeBias < 0.9) {
             type = 'Income'
         } else {
-            type = faker.helpers.arrayElement(['Investment', 'Saving'])
+            type = faker.helpers.arrayElement(['Saving', 'Investment'])
         }
 
         let amount
-
         switch (type) {
             case 'Income':
-                amount = faker.number.int({ min: 2000, max: 9000 })
+                amount = faker.number.int({
+                    min: 2000,
+                    max: 9000,
+                })
                 break
             case 'Expense':
-                amount = faker.number.int({ min: 10, max: 1000 })
+                amount = faker.number.int({
+                    min: 10,
+                    max: 1000,
+                })
                 break
             case 'Investment':
             case 'Saving':
-                amount = faker.number.int({ min: 3000, max: 10000 })
+                amount = faker.number.int({
+                    min: 3000,
+                    max: 10000,
+                })
                 break
         }
 
@@ -48,15 +89,16 @@ async function seed() {
             type,
             description: faker.lorem.sentence(),
             category,
+            user_id,
         })
     }
 
-    const { error } = await supabase.from('transactions').insert(transactions)
+    const { error } = await (await supabase).from('transactions').insert(transactions)
 
     if (error) {
         console.error('Error inserting data')
     } else {
-        console.log('Data was inserted')
+        console.log(`${transactions.length} transactions stored`)
     }
 }
 
